5a8e9e24995ca40bb49699dc579733070bbd193e
denismd <markovdenis2012@gmail.com>
2017-08-09 13:30:14 +0500

    logger

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
 const logger = require('./logger/logger');
+var express = require('express');
+var app = express();
+
+logger.debug('1');
+logger.info('2');
+logger.warn('3');
+logger.error('4');
--- server/logger/logger.js
+++ server/logger/logger.js
@@ -1,25 +1,34 @@
-var winston = require('winston');
+const winston = require('winston');
+const env = require('../server-enviroment');
+
+let transports = [
+    new (winston.transports.Console)({
+        handleExceptions: true,
+        humanReadableUnhandledException: true
+    }),
+    new (winston.transports.File)({
+        name: 'info-file',
+        filename: `logs/server-${env.minSeverity}.log`,
+        level: env.minSeverity,
+        handleExceptions: true
+    }),
+    new (winston.transports.File)({
+        name: 'warning-file',
+        filename: 'logs/server-only-warnings.log',
+        level: 'warn',
+        handleExceptions: true
+    }),
+    new (winston.transports.File)({
+        name: 'error-file',
+        filename: 'logs/server-only-errors.log',
+        level: 'error',
+        handleExceptions: true
+    })
+];
 
 winston.configure({
     level: 'debug',
-    transports: [
-        new (winston.transports.Console)({
-            handleExceptions: true,
-            humanReadableUnhandledException: true
-        }),
-        new (winston.transports.File)({
-            name: 'info-file',
-            filename: 'logs/server-info.log',
-            level: 'info',
-            handleExceptions: true
-        }),
-        new (winston.transports.File)({
-            name: 'error-file',
-            filename: 'logs/server-error.log',
-            level: 'error',
-            handleExceptions: true
-        })
-    ]
+    transports: transports
 });
 
 
