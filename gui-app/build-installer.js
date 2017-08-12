1ffcaceff1fd42126505a7aabf5ed6b0852eb37e
Тимофей <timon@rtural.ru>
2017-08-12 14:59:00 +0500

    sfsd

--- gui-app/build-installer.js
+++ gui-app/build-installer.js
@@ -1,9 +1,21 @@
 var electronInstaller = require('electron-winstaller');
 
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: './',
-    outputDirectory: './dist',
-    authors: 'DenisMD',
-    exe: 'gameminer.exe'
+// In this case, we can use relative paths
+var settings = {
+    // Specify the folder where the built app is located
+    appDirectory: './GameMiner-win32-x64',
+    // Specify the existing folder where
+    outputDirectory: './installer',
+    // The name of the Author of the app (the name of your company)
+    authors: 'Our Code World Inc.',
+    // The name of the executable of your built
+    exe: './GameMiner.exe'
+};
+
+resultPromise = electronInstaller.createWindowsInstaller(settings);
+
+resultPromise.then(() => {
+    console.log("The installers of your application were succesfully created !");
+}, (e) => {
+    console.log(`Well, sometimes you are not so lucky: ${e.message}`)
 });
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
