e2db42f0dc36825bf5adad2fc92b6b31d8d38dcb
DenisMD <markovdenis2012@gmail.com>
2017-08-13 23:49:12 +0500

    fonts

--- gui-app/src/js/angApp.js
+++ gui-app/src/js/angApp.js
@@ -1,34 +1,24 @@
-
-const packageInfo = require("../package");
-
+const packageInfo = require("./package");
 
 var mainApp = angular.module('game-miner-app', ['ui.router']);
-
-
 
 mainApp.config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-
-    // HOME STATES AND NESTED VIEWS ========================================
-        .state('red', {
-            url: '/red',
-            templateUrl: 'pages/red.html'
+        .state('initialize', {
+            url: '/',
+            templateUrl: 'pages/initialize.html'
         })
 
 });
 
-
 mainApp.controller('MainCtrl', function PhoneListController($scope,$http) {
     $scope.version = packageInfo.version;
 
-    $http.get("http://localhost:13555/info")
+    $http.get(`${packageInfo.externalServer}/info`)
         .then(function(response) {
             $scope.serverInfo = response.data;
         });
 });
-
-
-
