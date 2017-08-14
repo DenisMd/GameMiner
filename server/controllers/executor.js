aa33f02003299cf3b59db36022f35f5c9a6b0481
denismd <markovdenis2012@gmail.com>
2017-08-14 13:29:59 +0500

    mongo

--- server/controllers/executor.js
+++ server/controllers/executor.js
@@ -2,12 +2,23 @@ const express = require('express');
 const logger = require('../logger/logger');
 const env = require('../server-enviroment');
 
+const MongoClient = require('mongodb').MongoClient;
+
 // Routes
 const systemRoute = require('./system/system');
 
 module.exports = function () {
-    const app = express();
-    logger.info(`Starting server on port ${env.port}`);
-    systemRoute(app);
-    app.listen(env.port);
+    // Use connect method to connect to the server
+    MongoClient.connect(env.dburl, function(err, db) {
+        if (!err) {
+            logger.info("Connected successfully to mongodb");
+
+            const app = express();
+            logger.info(`Starting server on port ${env.port}`);
+            systemRoute(app);
+            app.listen(env.port);
+
+        } else
+            logger.error("Connetion to mongodb failed %j", err);
+    });
 };
--- server/controllers/system/system.js
+++ server/controllers/system/system.js
@@ -2,7 +2,7 @@ const env = require('../../server-enviroment');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.send("App works");
+        res.send("GServer works");
     });
     app.get('/info', function (req, res) {
         res.send(env.info)
