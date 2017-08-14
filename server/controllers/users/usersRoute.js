f09cc4caaef92fff6c0f548ef8497ba26e4ee59d
DenisMD <markovdenis2012@gmail.com>
2017-08-14 21:58:41 +0500

    создание пользователя

--- server/controllers/users/usersRoute.js
+++ server/controllers/users/usersRoute.js
@@ -1,5 +1,40 @@
+var uuid = require('uuid/v4');
+
 module.exports = function (app, db) {
-    app.get('/user/create', function (req, res) {
-        res.send("GServer works");
+
+    const guser = db.collection('guser');
+    const seq = db.collection('sequences');
+    seq.insertOne({name:"workerSeq", seq: 0});
+
+    function getNextWorker(name) {
+        var ret = seq.findAndModify(
+            {
+                query: {name: name},
+                update: { $inc: { seq: 1 } },
+                new: true
+            }
+        ).catch((err)=>{
+            console.log(err);
+        });
+
+        return ret.seq;
+    }
+
+    app.get('/user/new', function (req, res) {
+        const newUser = {};
+        newUser.uuid = uuid();
+        newUser.createDate = new Date();
+        newUser.nickname = "Аноним";
+        newUser.ip = [req.headers['x-forwarded-for'] || req.connection.remoteAddress];
+        newUser.enable = false;
+        newUser.workerId = getNextWorker("workerSeq");
+        newUser.systemInfo = {};
+
+        guser.insertOne(newUser)
+            .then(
+                (result) => {
+                    res.send(result.ops[0]);
+                }
+            );
     });
 };
